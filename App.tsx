4fbd6b7824d3c9703d462aa3228f0ffdb38e7f0d
Ariel Caminos <caminosariel@gmail.com>
2023-02-04 09:48:56 -0300

    relative, absolute and AlignSelf

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { SafeAreaView } from 'react-native'
+import { AlignSelf } from './src/screens/AlignSelf'
 import { FlexScreen } from './src/screens/FlexScreen'
+import { RelativePosition } from './src/screens/RelativePosition'
 
 const App = () => {
   return (
     <SafeAreaView style={{flex:1}}>
-      <FlexScreen/>
+      {/* <FlexScreen/> */}
+      {/* <RelativePosition/> */}
+      <AlignSelf/>
     </SafeAreaView>
   )
 }
